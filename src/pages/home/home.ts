41ecd1080abcbd8117b6e7e7e90c2712736df75c
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-19 00:46:27 +0000

    Create home page and change colour scheme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,9 +3,41 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+
 })
+
+
+
 export class HomePage {
+
+  revealInfo(){
+    const info = document.getElementById('information');
+    const work = document.getElementById('work-hours');
+    const price = document.getElementById('prices');
+
+    work.classList.add('hidden');
+    price.classList.add('hidden');
+    info.classList.remove('hidden');
+
+  }
+  revealWork(){
+    const info = document.getElementById('information');
+    const work = document.getElementById('work-hours');
+    const price = document.getElementById('prices');
+    work.classList.remove('hidden');
+    price.classList.add('hidden');
+    info.classList.add('hidden');
+  }
+  revealPrice(){
+    const info = document.getElementById('information');
+    const work = document.getElementById('work-hours');
+    const price = document.getElementById('prices');
+    work.classList.add('hidden');
+    price.classList.remove('hidden');
+    info.classList.add('hidden');
+  }
+
 
   constructor(public navCtrl: NavController) {
 
